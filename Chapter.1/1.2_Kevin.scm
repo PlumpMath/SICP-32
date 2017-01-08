41578edb8adc3473bae9a8e4dcb6dbe276979d52
Kevin <kevin@scalechain.io>
2017-01-08 11:11:55 +0900

    Finished 1.2

--- Chapter.1/1.2_Kevin.scm
+++ Chapter.1/1.2_Kevin.scm
@@ -306,7 +306,7 @@ Applicative order
 
 
 ;Testing for Primality
-
+#|
 (define (smallest-divisor n) (find-divisor n 2))
 
 (define (find-divisor n test-divisor)
@@ -327,12 +327,68 @@ Applicative order
         (else
          (remainder
           (* base (expmod base (- exp 1) m)) m))))
+|#
 
 ;Exercise 1.21
+#|
 (println "Exercise 1.21")
 (smallest-divisor 199)
 (smallest-divisor 1999)
 (smallest-divisor 19999)
+|#
+
+;Exercise 1.25
+(println "Exercise 1.25")
+(display "Alyssa P. Hacker complains that we went to a lot of extra work in writing expmod.
+After all, she says, since we already know how to compute exponentials, we could have simply written
+(define (expmod base exp m) (remainder (fast-expt base exp) m))
+Is she correct? Would this procedure serve as well for our fast prime tester? Explain.")
+(define (expmod base exp m) (remainder (fast-expt base exp) m))
+
+;Exercise 1.23
+(println "Exercise 1.23")
+(display "Define a procedure next that re- turns 3 if its input is equal to 2 and otherwise returns its in- put plus 2.
+Modify the smallest-divisor procedure to use (next test-divisor) instead of (+ test-divisor 1).")
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+#|
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m)) m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m)) m))))
+|#
+
+(define (next divisor)
+  (if (= divisor 2) 3 (+ divisor 2)))
+
+;Exercise 1.24
+(println "Exercise 1.24")
+(display "Modify the timed-prime-test procedure of Exercise 1.22 to use fast-prime? (the Fermat method),
+and test each of the 12 primes you found in that exercise. Since the Fermat test has Θ(log n) growth, how would
+ you expect the time to test primes near 1,000,000 to compare with the time needed to test primes near 1000?
+Do your data bear this out? Can you explain any discrepancy you find?")
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
 
 ;Exercise 1.22
 (println "Exercise 1.22")
@@ -342,8 +398,17 @@ Applicative order
   (display n)
   (start-prime-test n (current-inexact-milliseconds)))
 
+#|
+(println "Using prime?")
 (define (start-prime-test n start-time)
   (if (prime? n)
+      (report-prime (- (current-inexact-milliseconds) start-time))
+      #f))
+|#
+
+(println "Using fast-prime?")
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 4)
       (report-prime (- (current-inexact-milliseconds) start-time))
       #f))
 
@@ -353,7 +418,7 @@ Applicative order
   #t)
 
 (define (search-for-primes from to count)
-  (cond ((or (= count 3) (= from to)) (display "END"))
+  (cond ((or (= count 3) (= from to)) (display "\nEND"))
         ((even? from) (search-for-primes (+ from 1) to count))
         (else (if (timed-prime-test from) (search-for-primes (+ from 2) to (+ count 1))
                   (search-for-primes (+ from 2) to count)))))
@@ -361,3 +426,6 @@ Applicative order
 (search-for-primes 1000 10000 0)
 (search-for-primes 10000 100000 0)
 (search-for-primes 100000 1000000 0)
+(search-for-primes 1000000 10000000 0)
+
+
