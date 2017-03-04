d2692750f31369ec6d255ded9d76b29d723c5505
JooeunAhn <jflow@hanyang.ac.kr>
2017-03-05 01:43:57 +0900

    fix 1.43

--- Chapter.1/JooEun/1.43_JooEun.rkt
+++ Chapter.1/JooEun/1.43_JooEun.rkt
@@ -1,11 +1,17 @@
 #lang racket
 (define (compose f g) (lambda (x) (f (g x))))
 (define (square x) (* x x))
+(define (inc x) (+ x 1))
 
 (define (repeated f n)
-  (cond ((<= n 0) #false)
-        ((= n 1) f)
-        (else (repeated (compose f f) (- n 1)))
-        ))
+  (define (repeated-iter g n)
+    (if (= n 1)
+        g
+        (repeated-iter (compose f g) (- n 1))))
+  (if (<= n 0)
+      #false
+  (repeated-iter f n)))
 
-((repeated square 2) 5)
+((repeated square 2) 5);625
+((repeated inc 4) 5);9
+((repeated inc 0) 5);false
