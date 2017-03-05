427ad386c61387fdcbf23ae6333065fddc0882eb
Changhyun Lee <chang12@snu.ac.kr>
2017-03-06 02:10:28 +0900

    [LCH] 오늘 스터디 시간에 논의한 내용을 반영 (1.41, 2.76)

--- Chapter.1/1.41_LCH.rkt
+++ Chapter.1/1.41_LCH.rkt
@@ -1,8 +1,20 @@
 #lang racket
 
 (define (inc x) (+ x 1))
+(define (square x) (* x x))
 
 (define (double proc)
   (lambda (x) (proc (proc x))))
 
-(((double (double double)) inc) 5) ; 21 인게 신기하다!
+;(((double (double double)) inc) 5) ; 21 인게 신기하다!
+
+; d(d(d))
+; = d(dd)
+; = dddd
+; dddd(p)
+; = ddd(pp)
+; = dd(pppp)
+; = d(pppppppp)
+; = pppppppppppppppp
+; p = inc 이므로
+; +1 을 16번 하므로 5 + 16 = 21
--- Chapter.2/LCH/2.76_LCH.rkt
+++ Chapter.2/LCH/2.76_LCH.rkt
@@ -1,10 +1,20 @@
 #lang racket
 
 ; explicit dispatch / data-directed style / message-passing style 을 비교한다.
-; explicit dispatch 는 생각하고싶지 않다.
-; new operation / new type 이 추가될때 뒤의 두개는 추가해야할 작업이 비슷하지 않나 생각했다.
+; 세 가지 방식 모두 type 이 T개, operation 이 O개 있다고 했을때 이를 표현하기 위한 코드의 양은 T*O 수준으로 비슷하다고 생각한다.
+; 그러므로 더 나은 방법이다를 가르는 기준은, 코드를 추가할때 기존 코드를 건드리냐/아니냐로 갈린다고 생각한다.
 
-; data-directed style : new operation 이 추가되면 install 하는 코드마다 procedure 가 하나씩 추가된다.
-;                       new type 이 추가되면 install 하는 덩어리 procedure 가 하나 추가된다.
-; message-passing style : new operation 이 추가되면 "intelligent data object" 마다 procedure 를 하나 추가한다.
-;                         new type 이 추가되면 "intelligent data object" 이 하나 더 생긴다.                          
+; explicit dispatch 는 functional programming 과 유사하다.
+; 새로운 operation 을 추가하는건 기존 코드를 수정하지 않아도 된다.
+; 그냥 새로운 operation 을 나타내는 procedure 를 추가로 정의한다. 이 procedure 에서 type 에 따른 분기가 이뤄진다.
+; 새로운 type 을 추가하기 위해서는 기존의 모든 procedure(operation) 을 수정해야 한다.
+
+; data-directed style 은 새로운 느낌이다.
+; 새로운 operation / type 을 모두 기존 코드를 건드리지않고 수행할 수 있다.
+; 새로운 operation 을 추가할때는 각 type 에 대한 install procedure 를 수정해야한다고 생각했다.
+; 하지만 꼭 그럴 필요는 없다. 같은 table 에 대한 put/get 을 할 수 있다면, install-type 은 그대로 두고, install-type-addition procedure 를 새롭게 정의해도 된다.
+; 새로운 type 을 추가할때는 새로운 install-type procedure 를 정의하면 된다.
+
+; message-passing style 은 object-oriented programming 과 유사하다.
+; 새로운 type 을 추가하는건 새로운 intelligent data object 를 나타내는 procedure 를 추가하는 것이므로 기존 코드를 건드리지 않는다.
+; 하지만 새로운 operation 을 추가하기 위해서는 type 을 나타내는 procedure 마다 코드를 추가해줘야하므로 기존 코드를 건드린다.
