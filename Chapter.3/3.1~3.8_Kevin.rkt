f4b8ccfaf76c4b96b7c664c0c2f25b5fb9538661
Kevin <kevin@scalechain.io>
2017-03-25 20:29:28 +0900

    Add 3.1~3.8

--- Chapter.3/3.1~3.8_Kevin.rkt
+++ Chapter.3/3.1~3.8_Kevin.rkt
@@ -1,5 +1,6 @@
 #lang racket
-;3. Modularity, Objects and State
+
+;3. Modularity, Objects and State (local state variable)
 #|
 The first organizational strategy concentrates on objects, viewing a large system as a collection
 of distinct objects whose behaviors may change over time. An alternative organizational strategy
@@ -13,10 +14,15 @@ views a signal-processing system.
              balance)
       "Insufficient funds"))
 
+;This is a new kind of behavior for a procedure. Until now, all our procedures could be viewed as
+;speciﬁcations for comput- ing mathematical functions. A call to a procedure computed the value of
+;the function applied to the given arguments, and two calls to the same procedure with the same
+;arguments always produced the same result.
+
 ;(set! ⟨name⟩ ⟨new-value⟩)
 #|
 (begin ⟨exp1⟩ ⟨exp2⟩ . . . ⟨expk⟩)
-causes the expressions ⟨exp1⟩ through ⟨expk⟩ to be evaluated in se- quence and the value of the final
+causes the expressions ⟨exp1⟩ through ⟨expk⟩ to be evaluated in sequence and the value of the final
 expression ⟨expk ⟩ to be returned as the value of the entire begin form.
 |#
 (define new-withdraw
@@ -35,7 +41,7 @@ expression ⟨expk ⟩ to be returned as the value of the entire begin form.
         "Insufficient funds")))
 
 #|
-We can also create objects that handle deposits as well as with- drawals, and thus we can
+We can also create objects that handle deposits as well as withdrawals, and thus we can
 represent simple bank accounts. Here is a procedure that returns a “bank-account object” with
 a specified initial balance:
 |#
@@ -100,7 +106,8 @@ then mf returns the value of the counter. If the input is the special symbol res
 For any other input, mf returns the result of calling f on that input and increments the counter. For instance, we could make a
 monitored version of the sqrt procedure:
 
-(define s (make-monitored sqrt)) (s 100)
+(define s (make-monitored sqrt))
+(s 100)
 10
 (s 'how-many-calls?)
 1
@@ -122,7 +129,8 @@ monitored version of the sqrt procedure:
 (s 'how-many-calls?)
 
 #|
-The following expression creates and gives initial values to the two variables zebra and tiger. The body of the let expression is a list which calls the message function.
+The following expression creates and gives initial values to the two variables zebra and tiger.
+The body of the let expression is a list which calls the message function.
 
 (let ((zebra "stripes")
       (tiger "fierce"))
@@ -156,8 +164,10 @@ and should otherwise return a complaint:
   (define (dispatch pw m)
     (if (not (eq? pw password))
         (error "Incorrect password")
-        (cond ((eq? m 'withdraw) withdraw)
+        (cond
+          ((eq? m 'withdraw) withdraw)
           ((eq? m 'deposit) deposit)
+          ((eq? m 'is-valid-password) (eq? pw password))
           (else (error "Unknown request: MAKE-ACCOUNT"
                        m)))))
   dispatch)
@@ -167,8 +177,8 @@ and should otherwise return a complaint:
 ((acc2 'secret 'withdraw) 40)
 
 #|
-Exercise 3.4: Modify the make-account procedure of Ex- ercise 3.3 by adding another local state variable so that, if an account
-is accessed more than seven consecutive times with an incorrect password, it invokes the procedure call- the-cops.
+Exercise 3.4: Modify the make-account procedure of Exercise 3.3 by adding another local state variable so that, if an account
+is accessed more than seven consecutive times with an incorrect password, it invokes the procedure call-the-cops.
 |#
 (println "Exercise 3.4")
 (define (cops-make-account balance password)
@@ -207,30 +217,30 @@ is accessed more than seven consecutive times with an incorrect password, it inv
 ;((acc3 'donno 'withdraw) 40)
 
 #|
-It is tempting to conclude this discussion by saying that, by intro- ducing assignment and the technique of hiding state in local
+It is tempting to conclude this discussion by saying that, by introducing assignment and the technique of hiding state in local
 variables, we are able to structure systems in a more modular fashion than if all state had to be manipulated explicitly, by passing
  additional parameters. Unfortunately, as we shall see, the story is not so simple.
 |#
 
 #|
-Exercise 3.5: Monte Carlo integration is a method of esti- mating definite integrals by means of Monte Carlo simula- tion. Consider
-computing the area of a region of space de- scribed by a predicate P(x,y) that is true for points (x,y) in the region and false for
+Exercise 3.5: Monte Carlo integration is a method of estimating definite integrals by means of Monte Carlo simulation. Consider
+computing the area of a region of space described by a predicate P(x,y) that is true for points (x,y) in the region and false for
 points not in the region. For example, the region contained within a circle of radius 3 centered at (5, 7) is described by the
-predicate that tests whether (x - 5)2 + (y - 7)2 ≤ 32. To estimate the area of the region described by such a predicate, begin by
+predicate that tests whether (x - 5)^2 + (y - 7)^2 ≤ 32. To estimate the area of the region described by such a predicate, begin by
 choosing a rectangle that contains the region. For example, a rectangle with diagonally opposite corners at (2, 4) and (8, 10)
-con- tains the circle above. e desired integral is the area of that portion of the rectangle that lies in the region. We can
+con- tains the circle above. The desired integral is the area of that portion of the rectangle that lies in the region. We can
 estimate the integral by picking, at random, points (x,y) that lie in the rectangle, and testing P(x,y) for each point to determine
 whether the point lies in the region. If we try this with many points, then the fraction of points that fall in the region should
 give an estimate of the proportion of the rectangle that lies in the region. Hence, multiplying this fraction by the area of the
 entire rectangle should produce an estimate of the integral.
 
-Implement Monte Carlo integration as a procedure estimate- integral that takes as arguments a predicate P, upper and lower bounds
-x1, x2, y1, and y2 for the rectangle, and the number of trials to perform in order to produce the esti- mate. Your procedure should
-use the same monte-carlo procedure that was used above to estimate π . Use your estimate- integral to produce an estimate of π by
+Implement Monte Carlo integration as a procedure estimate-integral that takes as arguments a predicate P, upper and lower bounds
+x1, x2, y1, and y2 for the rectangle, and the number of trials to perform in order to produce the estimate. Your procedure should
+use the same monte-carlo procedure that was used above to estimate π . Use your estimate-integral to produce an estimate of π by
 measuring the area of a unit circle.
 
-You will find it useful to have a procedure that returns a number chosen at random from a given range. e follow- ing
-random-in-range procedure implements this in terms of the random procedure used in Section 1.2.6, which re-turns a nonnegative
+You will find it useful to have a procedure that returns a number chosen at random from a given range. The following
+random-in-range procedure implements this in terms of the random procedure used in Section 1.2.6, which returns a nonnegative
 number less than its input.
 
 (define (random-in-range low high) (let ((range (- high low)))
@@ -278,13 +288,71 @@ number less than its input.
 
 #|
 Exercise 3.6: It is useful to be able to reset a random-number generator to produce a sequence starting from a given value.
-Design a new rand procedure that is called with an ar- gument that is either the symbol generate or the symbol reset and behaves
-as follows: (rand 'generate) produces a new random number; ((rand 'reset) ⟨new-value ⟩) re- sets the internal state variable to the
-designated ⟨new-value ⟩. Thus, by resetting the state, one can generate repeatable se- quences. These are very handy to have when
+Design a new rand procedure that is called with an argument that is either the symbol generate or the symbol reset and behaves
+as follows: (rand 'generate) produces a new random number; ((rand 'reset) ⟨new-value ⟩) resets the internal state variable to the
+designated ⟨new-value ⟩. Thus, by resetting the state, one can generate repeatable sequences. These are very handy to have when
 testing and debugging programs that use random numbers.
 |#
 (println "Exercise 3.6")
+(define (make-rand-generator seed-number)
+  (define (dispatch m)
+    (cond ((eq? m 'generate)
+             (set! seed-number (rand-update seed-number))
+             seed-number)
+          ((eq? m 'reset)
+           (lambda (new-seed)
+             (set! seed-number new-seed)))))
+    dispatch)
 
+(define rand-new (make-rand-generator 5))
+(rand-new 'generate)
+(rand-new 'generate)
+(rand-new 'generate)
+
+((rand-new 'reset) 5)
+(rand-new 'generate)
 
   
-  
+#|
+Programming without any use of assignments, as we did throughout the ﬁrst two chapters of this book,
+is accordingly known as functional programming.
+
+In contrast to functional programming, programming that makes extensive use of assignment is known
+ as imperative programming. In addition to raising complications about computational models, programs
+written in imperative style are susceptible to bugs that cannot occur in functional programs.
+
+The complexity of imperative programs becomes even worse if we consider applications in which several
+processes execute concurrently.
+|#
+
+;Exercise 3.7
+(println "Exercise 3.7")
+(define (make-joint account old-password new-password)
+  (define (joint-account pw m)
+    (if (not (or (eq? pw old-password) (eq? pw new-password)))
+        (error "Incorrect password")
+        (cond ((eq? m 'withdraw) (account old-password 'withdraw))
+              ((eq? m 'deposit) (account old-password 'deposit))
+              (else (error "Unknown request:" m)))))
+  (if (not (account old-password 'is-valid-password))
+      (error "Incorrect password")
+      joint-account))
+
+(define kevin-acc (make-joint acc2 'secret 'park))
+
+((kevin-acc 'secret 'deposit) 100)
+((kevin-acc 'park 'withdraw) 60)
+        
+;Exercise 3.8
+(println "Exercise 3.8")
+
+(define (f-generator)
+  (let ((called #f))
+    (lambda (num)
+      (cond ((not called)
+             (begin (set! called #t)
+                  num))
+            (else 0)))))
+
+(define f (f-generator))
+(+ (f 0) (f 1))
