893ef7bc4863ec80757a3108a0f31f322fc6a8e3
Changhyun Lee <chang12@snu.ac.kr>
2017-03-26 22:37:10 +0900

    [LCH] ~ 3.4

---
+++ Chapter.3/LCH/3.02_LCH.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (make-monitored f)
+  (let ((count 0))
+    (lambda (arg)
+      (cond ((eq? arg 'how-many-calls?) count)
+            ((eq? arg 'reset-count) (set! count 0))
+            (else (begin (set! count (+ count 1))
+                         (f arg)))))))
+
+(define s (make-monitored sqrt))
+(s 100) ; 10
+(s 'how-many-calls?) ; 1
+(s 'reset-count)
+(s 'how-many-calls?) ; 0
